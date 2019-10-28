ffa3708c20345a8cf3cc2df7afaa12591e2d78ed
Explodeus <volodya.sokha@gmail.com>
2019-10-28 19:33:52 +0200

    Request module added

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const TelegramBot = require('node-telegram-bot-api');
-
+const request = require('request');
 
 const token = '962874264:AAFWRHEXHNmEmosluj36NXXOH06eKk6Hzak';
 
-
 const bot = new TelegramBot(token, {polling: true});
 
-
-bot.onText(/\/curse (.+)/, (msg, match) => {
+bot.onText(/\/curse/, (msg, match) => {
 
     const chatId = msg.chat.id;
 
